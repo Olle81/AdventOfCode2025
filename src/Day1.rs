08a254eaddda88da330b0233106681c6924d5825
Olov Johansson <olov.johansson@b3.se>
2025-12-06 22:20:51 +0100

    Day 1 part 2

--- src/Day1.rs
+++ src/Day1.rs
@@ -1,9 +1,66 @@
-use std::fs;
+use std::{fs, io};
 
 #[derive(Debug, Clone)]
 enum Move {
     Left(i32),
     Right(i32),
+}
+
+struct KeyLock {
+    position: i32,
+    code: i32,
+}
+
+impl KeyLock {
+    fn new() -> Self {
+        KeyLock {
+            position: 50,
+            code: 0,
+        }
+    }
+
+    fn turn(&mut self, mv: &Move) {
+        match mv {
+            Move::Left(dist) => self.update_code(-dist),
+            Move::Right(dist) => self.update_code(*dist),
+        }
+    }
+
+    fn turn2(&mut self, mv: &Move) {
+        let (dist, direction) = match mv {
+            Move::Left(dist) => (dist, -1),
+            Move::Right(dist) => (dist, 1),
+        };
+
+        for _ in 0..*dist {
+            self.position += direction;
+            if self.position < 0 {
+                self.position = 99;
+            } else if self.position > 99 {
+                self.position = 0;
+            }
+
+            if self.position == 0 {
+                self.code += 1;
+            }
+        }
+    }
+
+    fn update_code(&mut self, dist: i32) {
+        let y = self.position + dist % 100;
+        let revolutions = dist.abs() / 100;
+        self.code += revolutions
+            + (if (y < 0 || y > 100) && self.position != 0 {
+                1
+            } else {
+                0
+            });
+        self.position = y.rem_euclid(100);
+
+        if self.position == 0 {
+            self.code += 1;
+        }
+    }
 }
 
 fn to_move(str: &str) -> Move {
@@ -17,7 +74,7 @@ fn to_move(str: &str) -> Move {
     }
 }
 
-fn compute_code(moves: &[Move]) -> i32 {
+fn compute_code_part1(moves: &[Move]) -> i32 {
     let mut x = 50;
     let mut code = 0;
     for mv in moves {
@@ -34,6 +91,27 @@ fn compute_code(moves: &[Move]) -> i32 {
     code
 }
 
+fn compute_code_part2(moves: &[Move]) -> i32 {
+    let mut key_lock = KeyLock::new();
+    let mut key_lock2 = KeyLock::new();
+
+    for mv in moves {
+        key_lock.turn(mv);
+        key_lock2.turn2(mv);
+        println!(
+            "After move {:?}: position1 = {}, code1 = {}, position2 = {}, code2 = {}",
+            mv, key_lock.position, key_lock.code, key_lock2.position, key_lock2.code
+        );
+
+        if key_lock.code != key_lock2.code {
+            let mut buf = String::new();
+            let _apa = io::stdin().read_line(&mut buf);
+        }
+    }
+
+    key_lock.code
+}
+
 pub fn solve() {
     let moves: Vec<Move> = fs::read_to_string("InputDay1.txt")
         .unwrap() // panic on possible file-reading errors
@@ -41,7 +119,9 @@ pub fn solve() {
         .map(to_move) // make each slice into a string
         .collect(); // gather them together into a vector
 
-    let code = compute_code(&moves);
+    let code_part1 = compute_code_part1(&moves);
+    let code_part2 = compute_code_part2(&moves);
 
-    print!("Code = {}", code);
+    println!("Part 1 answer = {}", code_part1);
+    println!("Part 2 answer = {}", code_part2);
 }
