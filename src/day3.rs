132c0a3c44073da7663d08bbd1ca6e1313482780
Olov Johansson <olov.johansson@b3.se>
2025-12-13 11:03:04 +0100

    Day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,27 +1,28 @@
 use std::fs;
 
-fn get_voltage(battery: Vec<i8>) -> i8 {
-    let mut first_max_digit_index: usize = 0;
+fn get_joltage_2(battery: Vec<i8>, battery_length: usize) -> u64 {
+    let mut index_of_largest: usize = 0;
+    let mut result = 0;
 
-    for index in 0..battery.len() - 1 {
-        if battery[index] > battery[first_max_digit_index] {
-            first_max_digit_index = index;
+    for digit in 0..battery_length {
+        let end = battery.len() - battery_length + digit + 1;
+
+        for index in index_of_largest..end {
+            if battery[index] > battery[index_of_largest] {
+                index_of_largest = index;
+            }
         }
+
+        result = result * 10 + battery[index_of_largest] as u64;
+
+        index_of_largest += 1;
     }
 
-    let mut second_max_digit_index: usize = first_max_digit_index + 1;
-
-    for index in second_max_digit_index..battery.len() {
-        if battery[index] > battery[second_max_digit_index] {
-            second_max_digit_index = index;
-        }
-    }
-
-    battery[first_max_digit_index] * 10 + battery[second_max_digit_index]
+    result
 }
 
-pub fn solve() {
-    let voltages: u32 = fs::read_to_string("InputDay3.txt")
+pub fn get_result(battery_length: usize) -> u64 {
+    let joltage: u64 = fs::read_to_string("InputDay3.txt")
         .unwrap()
         .lines()
         .map(|line| {
@@ -29,9 +30,14 @@ pub fn solve() {
                 .map(|c| c as i8 - '0' as i8)
                 .collect::<Vec<i8>>()
         })
-        .map(get_voltage)
-        .map(|v| v as u32)
+        .map(|v| get_joltage_2(v, battery_length))
+        .map(|v| v as u64)
         .sum();
 
-    println!("Result: {:?}", voltages);
+    return joltage;
+}
+
+pub fn solve() {
+    println!("Result 1: {:?}", get_result(2));
+    println!("Result 2: {:?}", get_result(12));
 }
