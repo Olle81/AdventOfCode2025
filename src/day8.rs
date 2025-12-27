18d6ca0eacf0378cda4e85fdd40a47cdb2ec4d45
Olov Johansson <olov.johansson@b3.se>
2025-12-27 13:05:21 +0100

    Day 8 part 2

--- src/day8.rs
+++ src/day8.rs
@@ -22,26 +22,26 @@ struct Measurement<'a> {
     distance: f64,
 }
 
-fn group(neighbours: Vec<Measurement<'_>>) -> usize {
-    let sorted: Vec<Measurement> = neighbours
+struct GroupResult<'a> {
+    groups: Vec<Vec<&'a Point>>,
+    last_connection_points: (&'a Point, &'a Point),
+}
+
+fn group<'a>(
+    measurements: &'a Vec<Measurement<'a>>,
+    connections: usize,
+    number_of_boxes: usize,
+) -> GroupResult<'a> {
+    let sorted: Vec<&Measurement> = measurements
         .into_iter()
         .sorted_by(|a, b| a.distance.partial_cmp(&b.distance).unwrap())
         .collect();
 
-    // for g in sorted.iter() {
-    //     println!("{:?}", g);
-    // }
-
     let mut groups: Vec<Vec<&Point>> = vec![];
 
-    let print = |grp: &Vec<Vec<&Point>>| {
-        // for g in grp.iter() {
-        //     println!("{:?}", g);
-        // }
-        // println!();
-    };
+    let mut last_connected_pair: Option<(&Point, &Point)> = None;
 
-    for pair in sorted.iter().take(1000) {
+    for pair in sorted.iter().take(connections) {
         let (point_a, point_b) = (pair.point_a, pair.point_b);
 
         let has_a = groups
@@ -53,51 +53,34 @@ fn group(neighbours: Vec<Measurement<'_>>) -> usize {
 
         match (has_a, has_b) {
             (Some(idx_a), None) => {
-                //print!("Adding {} to group\n", point_b.index);
                 groups[idx_a].push(point_b);
-                print(&groups);
             }
             (None, Some(idx_b)) => {
-                //print!("Adding {} to group\n", point_a.index);
                 groups[idx_b].push(point_a);
-                print(&groups);
             }
             (None, None) => {
-                // print!(
-                //     "Creating new group with {} and {}\n",
-                //     point_a.index, point_b.index
-                // );
                 groups.push(vec![point_a, point_b]);
-                print(&groups);
             }
             (Some(idx_a), Some(idx_b)) => {
-                if idx_a == idx_b {
-                    // print!(
-                    //     "Both {} and {} are already in the same group\n",
-                    //     point_a.index, point_b.index
-                    // );
-                    // println!();
-                } else {
-                    // print!(
-                    //     "Merging groups of {} and {}\n",
-                    //     point_a.index, point_b.index
-                    // );
+                if idx_a != idx_b {
                     let mut group_b = groups[idx_b].clone();
                     groups[idx_a].append(&mut group_b);
                     groups.remove(idx_b);
-                    print(&groups);
                 }
             }
         }
+
+        last_connected_pair = Some((point_a, point_b));
+
+        if groups.len() == 1 && groups[0].len() == number_of_boxes {
+            break;
+        }
     }
 
-    groups
-        .iter()
-        .map(|g| g.len())
-        .sorted_by(|a, b| b.cmp(a))
-        .take(3)
-        .reduce(|acc, x| acc * x)
-        .unwrap()
+    GroupResult {
+        groups: groups,
+        last_connection_points: last_connected_pair.unwrap(),
+    }
 }
 
 fn measure(junction_boxes: &Vec<Point>) -> Vec<Measurement<'_>> {
@@ -123,6 +106,26 @@ fn measure(junction_boxes: &Vec<Point>) -> Vec<Measurement<'_>> {
         .collect()
 }
 
+fn solve_part_1(boxes: &Vec<Point>, connections: usize) -> usize {
+    let measures = measure(&boxes);
+    let result = group(&measures, connections, boxes.len());
+    result
+        .groups
+        .iter()
+        .map(|g| g.len())
+        .sorted_by(|a, b| b.cmp(a))
+        .take(3)
+        .reduce(|acc, x| acc * x)
+        .unwrap()
+}
+
+fn solve_part_2(boxes: &Vec<Point>) -> i64 {
+    let measures = measure(&boxes);
+    let groups = group(&measures, measures.len(), boxes.len());
+    let (p1, p2) = groups.last_connection_points;
+    p1.x * p2.x
+}
+
 pub fn solve() {
     let input: Vec<Point> = fs::read_to_string("InputDay8.txt")
         .unwrap()
@@ -139,8 +142,9 @@ pub fn solve() {
         })
         .collect();
 
-    let measures = measure(&input);
-    let result = group(measures);
+    let result_part_1 = solve_part_1(&input, 1000);
+    println!("Result part 1: {:?}", result_part_1);
 
-    println!("{:?}", result);
+    let result_part_2 = solve_part_2(&input);
+    println!("Result part 2: {:?}", result_part_2);
 }
